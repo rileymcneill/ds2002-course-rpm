2a9e5639579dbf449170f683d0ed6125147baeff
rileymcneill <mqt3uz@virginia.edu>
2024-02-23 12:25:04 -0500

    Add madlibs.sh script for Lab 3

---
+++ mywork/madlibs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Please give me an adjective: " ADJ1
+read -p "1. Please give me an noun: " N1
+read -p "1. Please give me an verb: " V1
+read -p "1. Please give me an adverb: " ADV1
+read -p "1. Please give me an adjective: " ADJ2
+read -p "1. Please give me an noun: " N2
+read -p "1. Please give me an verb: " V2
+read -p "1. Please give me an adverb: " ADV2
+
+echo "We appreciate your $ADJ1 availability to babysit tonight. The girls can watch an hour of $N1 before they $V1. Make sure they do it $ADV1 and tell them not to be $ADJ2 while doing so. They like to eat $N2 for dinner and they love to $V2 in the kitchen while you make it. Put them to bed $ADV2 after dinner."
